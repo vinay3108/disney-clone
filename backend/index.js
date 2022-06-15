c674a0c52c3d4ae1e9abae072949b16fa014c1be
vinay <vinaykr3108@gmail.com>
2022-06-15 21:09:41 +0530

    adding seedDB

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,21 @@
 const express=require('express');
 const app=express();
-const mongoose =require('mongoose');
 const dotenv = require( 'dotenv' );
 const cookieParser = require('cookie-parser');
-const bodyParser = require( 'body-parser' );
-mongoose.connect('mongodb://localhost:27017/minorDisney',{useNewUrlParser:true,useUnifiedTopology:true,})
-.then(()=>{
-    console.log("DB Connected");
-})
-.catch((err)=>{
-    console.log("OH NO ERROR !!");
-    console.log(err);
-})
+const connectDB=require('./db');
+// const seedDB=require('./seedDB');
+connectDB();
  
 app.use(express.urlencoded({extended:true}));
 app.use(express.json()); 
 app.use( cookieParser() );
 
-app.use( bodyParser.urlencoded( { extended: true } ) );
 dotenv.config({path:'backend/config/config.env'});
 
 const user=require('./routes/userRoutes');
 
 app.use( '/api/v1', user );
+// seedDB();
 
 const port=5000;
 
